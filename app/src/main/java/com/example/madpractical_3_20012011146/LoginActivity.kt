8dccf652c567637fdd6c0c5b026fba40aa26757d
Keyur Raval <ravalkeyur054@gmail.com>
2022-09-11 18:26:43 +0530

    COMMIT

--- app/src/main/java/com/example/madpractical_3_20012011146/LoginActivity.kt
+++ app/src/main/java/com/example/madpractical_3_20012011146/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madpractical_3_20012011146
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -7,14 +8,24 @@ import com.example.madpractical_3_20012011146.databinding.ActivityLoginBinding
 import com.example.madpractical_3_20012011146.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        binding.bottomNavigationView.selectedItemId = R.id.login
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId){
+                R.id.reg -> {
+                    Intent(this,RegistrationActivity::class.java).also{startActivity(it)}
+                }
+
+            }
+            return@setOnItemSelectedListener true
+        }
     }
+
 }
--- app/src/main/java/com/example/madpractical_3_20012011146/RegistrationActivity.kt
+++ app/src/main/java/com/example/madpractical_3_20012011146/RegistrationActivity.kt
@@ -1,20 +1,31 @@
 package com.example.madpractical_3_20012011146
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import com.example.madpractical_3_20012011146.databinding.ActivityLoginBinding
 import com.example.madpractical_3_20012011146.databinding.ActivityMainBinding
 import com.example.madpractical_3_20012011146.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var binding : ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        binding.bottomNavigationView.selectedItemId = R.id.reg
+        binding.bottomNavigationView.setOnItemSelectedListener { it1 ->
+            when(it1.itemId){
+                R.id.login -> {
+                    Intent(this,LoginActivity::class.java).also{startActivity(it)}
+                }
+
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
